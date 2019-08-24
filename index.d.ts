b4f98a41ca85ca631e4f04d5ff9d352dcadc93b6
tMinami <otherthan.new@gmail.com>
2019-08-25 00:38:23 +0900

    update

--- index.d.ts
+++ index.d.ts
@@ -2,19 +2,19 @@ declare module '*.vue' {
   import Vue, { ComponentOptions } from 'vue';
   // import bus from '~/plugins/eventBus.ts';
 
-  module 'vue/types/vue' {
-    interface Vue {
-      // $bus: any;
-      $nuxt: any;
-    }
-  }
+  // module 'vue/types/vue' {
+  //   interface Vue {
+  //     // $bus: any;
+  //     $nuxt: any;
+  //   }
+  // }
 
-  module 'vue/types/options' {
-    interface ComponentOptions<V extends Vue> {
-      // This adds the `middleware` property to the existing `vue/types/options/ComponentOptions` type
-      middleware?: string | string[];
-      layout?: string | string[];
-    }
-  }
-  export default Vue;
+  // module 'vue/types/options' {
+  //   interface ComponentOptions<V extends Vue> {
+  //     // This adds the `middleware` property to the existing `vue/types/options/ComponentOptions` type
+  //     middleware?: string | string[];
+  //     layout?: string | string[];
+  //   }
+  // }
+  // export default Vue;
 }
