d2304949ed612cad0b34e9df8dba6cf41f79b821
tMinami <otherthan.new@gmail.com>
2019-09-23 20:32:35 +0900

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -73,6 +73,20 @@ module.exports = {
         ignoreTemplateLiterals: true,
         ignoreRegExpLiterals: true
       }
-    ]
+    ],
+
+    // prettierの設定↓
+    // 'prettier/prettier': [
+    //   'error',
+    //   {
+    //     printWidth: 120,
+    //     tabWidth: 2,
+    //     semi: true,
+    //     singleQuote: true,
+    //     trailingComma: 'es5',
+    //     bracketSpacing: true,
+    //     useTabs: false
+    //   }
+    // ],
   }
 }
